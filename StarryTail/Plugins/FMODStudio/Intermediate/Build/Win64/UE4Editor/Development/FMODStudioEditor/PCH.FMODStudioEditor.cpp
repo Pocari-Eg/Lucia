8b47a88f6c3991d80bdc5b4c41c3393db3b39901
Sweat-Eg <66935108+Pocari-Eg@users.noreply.github.com>
2022-04-03 00:30:27 +0900

    수정 : main gitignore 수정

--- StarryTail/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudioEditor/PCH.FMODStudioEditor.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30133
-#include "C:/UnrealProjects/STARRY-TAIL/StarryTail/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudioEditor/PCH.FMODStudioEditor.h"
