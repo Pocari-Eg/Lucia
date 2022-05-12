a4338a6f27949f4b7e82c277150bbab7f624fc82
Hasdell <71982277+Hasdell@users.noreply.github.com>
2022-05-12 13:14:02 +0900

    추가 : 시엔티아 더미 애니메이션

---
+++ StarryTail/Source/StarryTail/EnemySource/Scientia/Scientia.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemySource/Monster.h"
+#include "Scientia.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STARRYTAIL_API AScientia : public AMonster
+{
+	GENERATED_BODY()
+	
+};
