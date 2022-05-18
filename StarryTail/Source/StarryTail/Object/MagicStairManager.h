d90189443695f3f0ccb0db9d4f18e504e665addb
Pocari-Eg <66935108+Pocari-Eg@users.noreply.github.com>
2022-05-18 16:26:25 +0900

    추가 : 마법 계단 투명효과
    임시 마법 계산 투명효과 추가

---
+++ StarryTail/Source/StarryTail/Object/MagicStairManager.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../StarryTail.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+
+#include "MagicStairTrigger.h"
+#include "MagicStairManager.generated.h"
+
+UCLASS()
+class STARRYTAIL_API AMagicStairManager : public AActor
+{
+	GENERATED_BODY()
+public:
+	//충돌 트리거
+	UPROPERTY(EditAnywhere, Category = Stair)
+	TArray<AActor*> StairArray;
+	
+
+	UPROPERTY(EditAnywhere, Category = Trigger)
+	AMagicStairTrigger* FirstTrigger;
+
+
+	UPROPERTY(EditAnywhere, Category = Trigger)
+	AMagicStairTrigger* SecondTrigger;
+
+private:
+	float min = 2000.0f;
+	TArray<UMaterialInstanceDynamic*> NewMaterial;
+public:	
+	// Sets default values for this actor's properties
+	AMagicStairManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	void MagicStairControl();
+};
