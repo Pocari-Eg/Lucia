19980a78b0daadd20d60c4cc339c9f9a10e6dddf
Pocari-Eg <66935108+Pocari-Eg@users.noreply.github.com>
2022-09-19 22:34:40 +0900

    수정 : 디버그 오류 수정

--- StarryTail/Source/StarryTail/EnemySource/BTService/BTServiceMobDetectPlayer.h
+++ StarryTail/Source/StarryTail/EnemySource/BTService/BTServiceMobDetectPlayer.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "BehaviorTree/BTService.h"
-#include "Components/LineBatchComponent.h"
 #include "BTServiceMobDetectPlayer.generated.h"
 
 /**
@@ -16,23 +15,9 @@ class STARRYTAIL_API UBTServiceMobDetectPlayer : public UBTService
 	GENERATED_BODY()
 public:
 	UBTServiceMobDetectPlayer();
-	FVector AngleToDir(float angle);
 	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 	
 
 	
 };
 
-ULineBatchComponent* GetDebugLineBatcher(const UWorld* InWorld, bool bPersistentLines, float LifeTime, bool bDepthIsForeground);
-
-static float GetDebugLineLifeTime(ULineBatchComponent* LineBatcher, float LifeTime, bool bPersistent);
- void DrawRadial(const UWorld* _InWorld,
-	const FMatrix& _TransformMatrix,
-	float _Radius,
-	float _Angle,
-	const FColor& _Color,
-	int32 _Segments,
-	float _LifeTime,
-	bool _bPersistentLines,
-	uint8 _DepthPriority,
-	float _Thickness);
