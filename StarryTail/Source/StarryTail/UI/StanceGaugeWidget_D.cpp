2c1312bb719a98e4f4e569e9f78f68a4e737ae40
DongWook482 <76018740+DongWook482@users.noreply.github.com>
2022-11-06 12:49:52 +0900

    윤동욱_IngameUI 퀄리티업

--- StarryTail/Source/StarryTail/UI/StanceGaugeWidget_D.cpp
+++ StarryTail/Source/StarryTail/UI/StanceGaugeWidget_D.cpp
@@ -4,6 +4,7 @@
 #include "StanceGaugeWidget_D.h"
 #include "Components/Image.h"
 #include "../PlayerSource/IreneCharacter.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UStanceGaugeWidget_D::ResetStanceGague()
 {
@@ -58,12 +59,72 @@ void UStanceGaugeWidget_D::NativeConstruct()
 	Irene = Cast<AIreneCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
 }
 
+void UStanceGaugeWidget_D::BrushSetColor(float _current)
+{
+	auto MAT = gauge_full->GetDynamicMaterial();
+	if (_current <= 0.0f)
+	{
+		if (gauge_fill->Brush.TintColor == FSlateColor(idlecolor)) return;
+		gauge_fill->SetBrushTintColor(FSlateColor(idlecolor));
+	}
+	else if (_current < 100.0f)
+	{
+		FLinearColor c;
+		MAT->GetVectorParameterValue(FHashedMaterialParameterInfo(), c);
+
+		if (c == fillcolor) return;
+		MAT->SetVectorParameterValue("ProgressColor1", fillcolor);
+		gauge_fill->SetBrushTintColor(FSlateColor(FLinearColor::White));
+	}
+	else if (_current >= 100.0f)
+	{
+		FLinearColor c;
+		MAT->GetVectorParameterValue(FHashedMaterialParameterInfo(), c);
+
+		if (c == fullcolor) return;
+		MAT->SetVectorParameterValue("ProgressColor1", fullcolor);
+		//SetShine();
+		//GetWorld()->GetTimerManager().SetTimer(stopshine, this, &UStanceGaugeWidget_D::SetShine, 2.0f, false, 2.0f);
+	}
+}
+
+void UStanceGaugeWidget_D::SetShine()
+{
+	SHINE = !SHINE;
+	if (SHINE)
+	{
+		targetR = 1.0f;
+		targetG = 0.838524f;
+		targetB = 0.128853f;
+	}
+	else
+	{
+		targetR = 0.875f;
+		targetG = 0.733709f;
+		targetB = 0.112746f;
+	}
+}
+
 void UStanceGaugeWidget_D::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
 {
 	Super::NativeTick(MyGeometry, InDeltaTime);
 
 	if (Irene == nullptr) return;
 
+	auto Current = Irene->IreneData.CurrentGauge;
+
+	BrushSetColor(Current);
+
 	auto MAT = gauge_full->GetDynamicMaterial();
-	MAT->SetScalarParameterValue("Progress", Irene->IreneData.CurrentGauge / 100.0f);
+	MAT->SetScalarParameterValue("Progress", Current / 100.0f);
+
+	/*if (SHINE)
+	{
+		FLinearColor c;
+		MAT->GetVectorParameterValue(FHashedMaterialParameterInfo(), c);
+		auto R = UKismetMathLibrary::FInterpTo(c.R, targetR, InDeltaTime, 10.0f);
+		auto G = UKismetMathLibrary::FInterpTo(c.G, targetG, InDeltaTime, 10.0f);
+		auto B = UKismetMathLibrary::FInterpTo(c.B, targetB, InDeltaTime, 10.0f);
+		MAT->SetVectorParameterValue("ProgressColor1", FLinearColor(R, G, B));
+	}*/
 }
